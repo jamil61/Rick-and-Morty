7378793b20e5d0780ad2bd6c65cadf9fab2d092d
jamil61 <jamisul6192@gmail.com>
2019-11-01 10:31:11 +0100

    rick

--- 1/menu.js
+++ 1/menu.js
@@ -3,7 +3,4 @@ $(document).ready(function(){
     $(".burger-nav").on("click", function(){
         $("header nav ul").toggleClass("open");
     })
-    $(".burger-nav").on("click", function(){
-        $("header nav ul").toggleClass("close");
-    })
 })
